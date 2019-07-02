6f8d1c0f0059c8b32b746058e3cb17b19af396e7
artem <gorbacartem90@gmail.com>
2019-07-02 17:02:33 +0300

    raw router

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,15 +2,34 @@ import React from 'react';
 import './Home.css';
 import Profile from "./../Profile/Profile";
 import Login from "./../Authentication/Login";
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
+
+
+function AppRouter() {
+  return (
+    
+    <Router>
+    <div className="home">
+    <Route path="/" component={Home} />
+    <Route path="/login" component={Login} />
+    <Route path="/profile" component={Profile} />
+
+    </div>
+    </Router>
+  );
+}
 
 function Home() {
   return (
-    <div className="home">
-        <div>Home</div>
-        <Profile />
-        <Login />
+    <div className="home-router">
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/profile">Profile</Link>
     </div>
   );
 }
 
-export default Home;
+
+
+
+export default AppRouter;
